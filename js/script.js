e33e234a2c86eaca53e089aa6d7b19f869f583ad
Layla Flight <layla.flight@greenschoolbrooklyn.com>
2018-01-16 23:28:48 +0000

    add

--- js/script.js
+++ js/script.js
@@ -6,24 +6,45 @@
 
 // Document Ready Function. All of your jQuery should go in here. 
 $( document ).ready(function() {
-  $("button").click(function(){
-    var message = $("input").val();
-    var pigMessage = ay(message);
-    $("#message").text(pigMessage);
-    console.log(pigMessage);
-});
+ $("button").click(function(){ 
+    sentenceToPigLatin() 
+ 
+ });
 
-function ay(word){
- return word + "ay";    
-}
-
+      
+   
 
 });
 
 
 // Create the wordToPigLatin function that takes a word as a parameter and returns a transfromed word. 
 
+function sentenceToPigLatin() {
+        console.log("Im here");
+ 	      var message=$("input").val();
+ 	     // var pigMessage = ay(message);
+       // $("#message").text(pigMessage);
+          var split= message.split(' ');
+ 	 
+ 	 for(var count=0; count<split.length; count=count+1){   
+ 	    var pigMessage = ay(split[count]);
+        $("#message").append(pigMessage);
+       
+   }
+ 
+ 
+}
 
+    
+function ay(word){
+var firstLetter = word.slice(0,1);
+var lastLetter = word.slice(1,word.length);
+if (firstLetter=='a'||firstLetter=='e'||firstLetter=='i'||firstLetter=='o'||firstLetter=='u'){ 
+ return  word + "ay ";    
+}else {
+ return lastLetter + firstLetter + "ay ";
+}
+}
 
 
 
@@ -31,5 +52,4 @@ function ay(word){
 // Create the sentenceToPigLatin function that takes a sentence as a parameter
 	//Loops through all the words in the sentence and transforms each word
 	//It should return a transfromed sentance
-
 
